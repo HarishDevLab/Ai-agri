1297561b1bc4ec0769520b1425328e23b2126ad5
HARISH <194191925+HarishDevLab@users.noreply.github.com>
2026-01-01 09:41:55 +0530

    This is .ipynb file

--- SCRIPT TO RUN WHISPER.py
+++ SCRIPT TO RUN WHISPER.py
@@ -1,3 +1,5 @@
+#LINUX 
+#THIS IS .ipynb FILE
 %%bash
 pip install git+https://github.com/openai/whisper.git 
 pip install --upgrade --no-deps --force-reinstall git+https://github.com/openai/whisper.git
